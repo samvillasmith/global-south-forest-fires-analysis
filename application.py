222c5312d8a94b4f76e75842a6c876dacdb673d9
samvillasmith <samuel.villa.smith@hotmail.com>
2024-10-12 19:11:06 -0500

    new deployment

--- application.py
+++ application.py
@@ -36,4 +36,4 @@ def predict_datapoint():
         return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=False) 
