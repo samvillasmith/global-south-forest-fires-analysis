817f75774379eaead79c2051411e7a227c6cc912
samvillasmith <samuel.villa.smith@hotmail.com>
2024-10-12 20:59:28 -0500

    debugging

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
-import pandas as pf
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
